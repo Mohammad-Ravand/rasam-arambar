5716b31321fdf301e5dd8d101b605997763e6aff
Mojtaba Gharibreza <mojtabaqri@gmail.com>
2024-01-16 10:20:18 +0330

    add splash screen

---
+++ pages/splash/splash.vue
@@ -0,0 +1,40 @@
+<script setup>
+</script>
+
+<template>
+
+<div class="bg-[#bd1e59] flex flex-col items-center justify-between min-h-screen p-4">
+  <div class="flex flex-col items-center justify-center pt-10">
+    <img
+      src="images/intro.webp"
+      alt="Logo"
+      class="mb-2"
+      width="200"
+      height="60"
+      style="aspect-ratio: 200 / 60; object-fit: cover;"
+    />
+    <h1 class="text-white text-2xl font-bold">آرام بار</h1>
+    <p class="text-white mt-1"> حمل و نقلی آسان و بی دردسر ...</p>
+  </div>
+  <div class="flex flex-col items-center justify-center">
+    <img
+    src="images/logo.png"
+      alt="Delivery Illustration"
+      width="200"
+      height="300"
+      style="aspect-ratio: 200 / 300; object-fit: cover;"
+    />
+  </div>
+  <div class="flex items-center justify-center space-x-2 pb-10">
+    <div class="w-2.5 h-2.5 bg-white rounded-full"></div>
+    <div class="w-2.5 h-2.5 bg-white rounded-full"></div>
+    <div class="w-2.5 h-2.5 bg-white rounded-full"></div>
+  </div>
+</div>
+ 
+</template>
+
+
+
+<style scoped>
+</style>
